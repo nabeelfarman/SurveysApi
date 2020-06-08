86910fb6771bb7ba14196501090145ebd482b1d3
nabeelfarman <knabeel380@gmail.com>
2020-06-08 23:07:03 +0500

    changes

---
+++ Models/individual.cs
@@ -0,0 +1,9 @@
+public class individual
+{
+    public int indvdlID { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string password { get; set; }
+    public string email { get; set; }
+    public int typeCd { get; set; }
+}
